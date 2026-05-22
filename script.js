0622d36035bdd1ce1e98db55dff3cb54adc0d343
Rafael Silva <rafaelsilva039133@gmail.com>
2026-05-21 22:20:46 -0300

    troca de svg por código

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function toggleQuestion(num) {
 
   div.style.height = "auto";
   div.style.padding = "5px";
-  trocarImagem(num + 1, "-");
+  trocarImagem(num + 1, "mostrar");
   isOpen[0] = num;
   isOpen[1] = true;
 }
@@ -39,10 +39,19 @@ function close() {
   const div = respostas.item(isOpen[0]);
   div.style.height = "0px";
   div.style.padding = "0px";
-  trocarImagem(isOpen[0] + 1, "+");
+  trocarImagem(isOpen[0] + 1, "esconder");
   isOpen[1] = false;
 }
 
 function trocarImagem(num, param) {
-  document.getElementById(`q${num}img`).src = `assets/button ${param}.svg`;
+  const mais = document.getElementById(`q${num}imgmais`);
+  const menos = document.getElementById(`q${num}imgmenos`);
+
+  if (param == "mostrar"){
+    mais.style.display = "none";
+    menos.style.display = "block";
+  } else if (param == "esconder"){
+    mais.style.display = "block";
+    menos.style.display = "none";
+  }
 }
